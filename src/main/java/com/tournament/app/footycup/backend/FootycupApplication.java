101caf8682e6958bf53f0688ea18a1e712f5016e
akknapik <arkadiusz.knapik.2002@gmail.com>
2025-03-06 16:59:38 +0100

    Initial commit

---
+++ src/main/java/com/tournament/app/footycup/backend/FootycupApplication.java
@@ -0,0 +1,13 @@
+package com.tournament.app.footycup.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootycupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootycupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tournament/app/footycup/FootycupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tournament.app.footycup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootycupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
