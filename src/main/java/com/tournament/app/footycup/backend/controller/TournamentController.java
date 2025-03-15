e035f992ca65adba5de5845f3acded222b462f52
akknapik <arkadiusz.knapik.2002@gmail.com>
2025-03-15 18:22:58 +0100

    feat: add endpoint for retrieving tournaments', fix request link and static data

--- src/main/java/com/tournament/app/footycup/backend/controller/TournamentController.java
+++ src/main/java/com/tournament/app/footycup/backend/controller/TournamentController.java
@@ -1,5 +1,7 @@
 package com.tournament.app.footycup.backend.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,11 +9,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/tournament")
+@RequestMapping("/tournaments")
 public class TournamentController {
     private static final Map<Integer, Map<String, String>> tournaments = new
             HashMap<>() {{
-                put(1, Map.of("id", "1", "name", "Dunajec Winter Cup", "startDate", "15.03.2025", "endDate", "15.03.2025", "id_organizer", "1", "teams", ""));
-                put(2, Map.of("id", "2", "name", "Glinik Winter Cup", "startDate", "20.03.2025", "endDate", "21.03.2025", "id_organizer", "2", "teams", ""));
+                put(1, Map.of("id", "1", "name", "Dunajec Winter Cup", "startDate", "15.03.2025"));
+                put(2, Map.of("id", "2", "name", "Glinik Winter Cup", "startDate", "20.03.2025"));
             }};
+
+    @GetMapping
+    public ResponseEntity<Object> getTournaments() {
+        return ResponseEntity.ok(tournaments.values());
+    }
 }
