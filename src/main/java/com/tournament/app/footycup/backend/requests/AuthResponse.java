35cfc01bfd8928460bc061b346481ff8b1e1f4a6
akknapik <arkadiusz.knapik.2002@gmail.com>
2025-04-02 22:31:49 +0200

    feat: add Login, Registration Request and AuthResponse implementation

---
+++ src/main/java/com/tournament/app/footycup/backend/requests/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.tournament.app.footycup.backend.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class AuthResponse {
+    public final String token;
+}
