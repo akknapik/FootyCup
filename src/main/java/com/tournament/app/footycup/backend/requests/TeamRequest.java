cba769e702854fcdd8a3b5adc9153159e278bf64
akknapik <arkadiusz.knapik.2002@gmail.com>
2025-05-07 17:51:05 +0200

    feat: add Units Tests for TeamService

--- src/main/java/com/tournament/app/footycup/backend/requests/TeamRequest.java
+++ src/main/java/com/tournament/app/footycup/backend/requests/TeamRequest.java
@@ -1,10 +1,12 @@
 package com.tournament.app.footycup.backend.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class TeamRequest {
     private String name;
     private String country;
